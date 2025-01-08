8deb5541d30821ddb834ebbd936cfbf93e48f14c
Juri <jurilikes2eatcurry@gmail.com>
2025-01-08 15:26:29 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
 // Play sound on navigation
 document.addEventListener("DOMContentLoaded", () => {
-    const navSound = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/drops-of-electricity-sfx-242334.mp3");
-    const hoverSound = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/drops-of-electricity-sfx-242334.mp3");
+    const navSound = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/scream-with-echo-46585.mp3");
 
     // Play sound on page load
     navSound.play();
 
-    // Add hover sound for links
-    document.querySelectorAll("a").forEach(link => {
-        link.addEventListener("mouseenter", () => hoverSound.play());
     });
 });
