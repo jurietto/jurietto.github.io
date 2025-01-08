9d5e9a2f9f74976365af8d31992feb27f31af892
Juri <jurilikes2eatcurry@gmail.com>
2025-01-08 15:38:36 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
-// Play sound on navigation
 document.addEventListener("DOMContentLoaded", () => {
     const navSound = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/scream-with-echo-46585.mp3");
 
-    // Play sound on page load
-    navSound.play();
-
+    // Play sound only when navigating to the "About Me" page
+    document.querySelectorAll("a").forEach(link => {
+        if (link.href.includes("about.html")) {
+            link.addEventListener("click", () => {
+                navSound.play();
+            });
+        }
     });
 });
+
