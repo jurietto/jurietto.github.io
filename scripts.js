6d5b487da3ffc2dfadd1b50da8ec7eff576dc1a5
Juri <jurilikes2eatcurry@gmail.com>
2025-01-08 18:10:31 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const musicPlayer = new Audio("https://www.example.com/audio-file.mp3"); // Replace with actual music URL
+    const musicPlayer = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/Velvet%20Acid%20Christ%20-%20Lust%20For%20Blood%20(2006)%20(Full%20Album)%20%5B%20ezmp3.cc%20%5D.mp3"); // Replace with actual music URL
     const audioControl = document.getElementById("audio-control");
 
     // Load music state from localStorage
