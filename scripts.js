c3450fac6175a0274cb3f695547bfa5203922e30
Juri <jurilikes2eatcurry@gmail.com>
2025-01-08 16:16:05 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const musicPlayer = document.getElementById("music-player");
+    const toggleButton = document.querySelector("img[alt='']");
+
+    // Toggle play/pause when the decorative image is clicked
+    toggleButton.addEventListener("click", () => {
+        if (musicPlayer.paused) {
+            musicPlayer.play();
+        } else {
+            musicPlayer.pause();
+        }
+    });
+});
 
 
