0db557134d197258f5628ddb843a579ab8baebf6
Juri <jurilikes2eatcurry@gmail.com>
2025-01-08 16:52:37 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Play sound when the About Me page is fully loaded
-    const navSound = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/scream-with-echo-46585.mp3");
-    navSound.play();
-
-    // Audio player toggle
-    const musicPlayer = new Audio("https://www.example.com/audio-file.mp3"); // Replace with actual URL
+    // Background music toggle
+    const musicPlayer = new Audio("https://file.garden/ZhTgSjrp5nAroRKq/Velvet%20Acid%20Christ%20-%20Lust%20For%20Blood%20(2006)%20(Full%20Album)%20%5B%20ezmp3.cc%20%5D.mp3"); // Replace with actual music URL
     const audioControl = document.getElementById("audio-control");
 
     if (audioControl) {
