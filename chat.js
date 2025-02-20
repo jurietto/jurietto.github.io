137b24311b0cdd17ce19e9238ff71f8b2b4ebe13
Juri <jurilikes2eatcurry@gmail.com>
2025-02-20 10:04:17 -0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -5,6 +5,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const notificationSound = document.getElementById("notification-sound");
     const uploadInput = document.getElementById("upload-input");
 
+    // Initialize Firebase
+    const firebaseConfig = {
+  apiKey: "AIzaSyB5TPELxjl-qo9v8Zt2k6aO0VGnxOcrecw",
+  authDomain: "dungeon-forum.firebaseapp.com",
+  databaseURL: "https://dungeon-forum-default-rtdb.firebaseio.com",
+  projectId: "dungeon-forum",
+  storageBucket: "dungeon-forum.firebasestorage.app",
+  messagingSenderId: "1073920232004",
+  appId: "1:1073920232004:web:15df0ccc5f3bf76a238a11"
+};
+    firebase.initializeApp(firebaseConfig);
+    const chatRef = firebase.database().ref("chats");
+
     if (messageInput) {
         messageInput.addEventListener("keypress", function(event) {
             if (event.key === "Enter" && !event.shiftKey) {
