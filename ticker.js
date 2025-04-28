1454afdcdd78e63f2af892597965818bea59f68b
Juri <jurilikes2eatcurry@gmail.com>
2025-04-28 03:57:12 -0700

    Create ticker.js

---
+++ ticker.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tickerHTML = `
+        <div class="ticker">
+            <div class="ticker-text">
+                WELCOME NETIZENS!!! ★ typing with squishy paws on my keyboard :3
+            </div>
+        </div>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', tickerHTML);
+});
