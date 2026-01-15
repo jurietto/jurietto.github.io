42b3387c1ca5757a28fd3057f5eeed5a41ffefed
Juri <jurilikes2eatcurry@gmail.com>
2026-01-15 13:47:33 -0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8cIAiNrasL-cgjQMcN0V-7s3kYdtiRjs",
+  authDomain: "chansi-ddd7e.firebaseapp.com",
+  projectId: "chansi-ddd7e",
+  storageBucket: "chansi-ddd7e.appspot.com",
+  appId: "1:650473918964:web:63be3d4f9794f315fe29a1"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
