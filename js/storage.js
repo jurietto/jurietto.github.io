80d4051ccdaa29d9c1b07f9af035e307ac2b308c
Juri <jurilikes2eatcurry@gmail.com>
2026-01-15 20:27:15 -0800

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,19 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { storage } from "./firebase.js";
 import {
-  getStorage,
   ref,
   uploadBytes,
   getDownloadURL
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-import { app } from "./firebase.js";
-
-const storage = getStorage(app);
 
 export async function uploadFile(file) {
   const path = `uploads/${Date.now()}_${file.name}`;
   const fileRef = ref(storage, path);
 
   await uploadBytes(fileRef, file);
-  return await getDownloadURL(fileRef);
+  const url = await getDownloadURL(fileRef);
+
+  return url;
 }
