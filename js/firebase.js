788ecb833a400c6f9898c8515c4353f2c978d48c
Juri <jurilikes2eatcurry@gmail.com>
2026-01-15 21:51:57 -0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,12 +5,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyA8cIAiNrasL-cgjQMcN0V-7s3kYdtiRjs",
   authDomain: "chansi-ddd7e.firebaseapp.com",
   projectId: "chansi-ddd7e",
-  storageBucket: "chansi-ddd7e.appspot.com",
+
+  // 🔴 THIS LINE WAS MISSING / WRONG BEFORE
+  storageBucket: "chansi-ddd7e.firebasestorage.app",
+
   appId: "1:650473918964:web:63be3d4f9794f315fe29a1"
 };
 
-/* 🔑 THIS WAS MISSING BEFORE */
 export const app = initializeApp(firebaseConfig);
-
-/* Firestore */
 export const db = getFirestore(app);
