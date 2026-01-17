743139d7931e40663efc33418b99f8a43b1e66cb
Juri <jurilikes2eatcurry@gmail.com>
2026-01-17 12:42:08 -0800

    Update changelog.js

--- js/changelog.js
+++ js/changelog.js
@@ -13,7 +13,7 @@ fetch(`https://api.github.com/repos/${OWNER}/${REPO}/commits`)
       const msg = c.commit.message.split("\n")[0];
 
       const line = document.createElement("div");
-      line.textContent = `🍭 ${msg} (・_・;)`;
+      line.textContent = `☆ ${msg} (・_・;)`;
 
       list.appendChild(line);
     });
